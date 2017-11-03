3f1dbb927d44b241b2494e5a5a94d8dceb86cc16
David Hilscher <davidhilscher@gmail.com>
2017-11-03 12:19:14 -0700

    sign up form added to login page

---
+++ imports/ui/containers/login/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import LoginWrapper from "../../components/LoginWrapper";
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="login-container">
+        <LoginWrapper />
+      </div>
+    );
+  }
+}
+
+export default Login;
