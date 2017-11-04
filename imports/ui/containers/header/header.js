c2ab0c3079ed1b88e979e04154791cc3959d91b3
David Hilscher <davidhilscher@gmail.com>
2017-11-03 17:09:49 -0700

    insecure & autopublish disabled

--- imports/ui/containers/header/header.js
+++ imports/ui/containers/header/header.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react'
-import AppBar from 'material-ui/AppBar'
-import HeaderLeft from './headerLeft'
-import HeaderRight from './headerRight'
+import React, { Component } from "react";
+import AppBar from "material-ui/AppBar";
+import HeaderLeft from "./headerLeft";
+import HeaderRight from "./headerRight";
 
-import './styles.css'
+import "./styles.css";
 
-const thisUrl = window.location.href
+const thisUrl = window.location.href;
 
 class HeaderContainer extends Component {
   render() {
     return (
-      <AppBar style={{height: 90}}> <div className="left"><HeaderLeft /></div><div className="right"><HeaderRight /></div> </AppBar>
-    )
+      <AppBar style={{ height: 90 }}>
+        {" "}
+        <div className="left">
+          <HeaderLeft />
+        </div>
+        <div className="right">
+          <HeaderRight />
+        </div>{" "}
+      </AppBar>
+    );
   }
 }
 
-export default HeaderContainer
+export default HeaderContainer;
