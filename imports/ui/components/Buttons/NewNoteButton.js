f711be11b6f24db002b0946c36eac68ea35e088f
Camden_Shaw <camden.shaw@gmail.com>
2017-11-05 20:53:27 -0800

    added links to buttons in home-page app-bar

--- imports/ui/components/Buttons/NewNoteButton.js
+++ imports/ui/components/Buttons/NewNoteButton.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton'
+import { Link } from 'react-router-dom'
 
 const NewNoteButton = props => {
   return (
-    <RaisedButton primary >
-      New Note
-    </RaisedButton>
+    <Link to='/note'>
+      <RaisedButton primary >
+        New Note
+      </RaisedButton>
+    </Link>
   )
 }
 
