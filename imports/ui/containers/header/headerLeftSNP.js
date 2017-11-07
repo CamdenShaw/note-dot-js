c3362a9c4b5752963cc5213518366c17c3be115f
David Hilscher <davidhilscher@gmail.com>
2017-11-07 10:40:32 -0800

    login page conditionally renders based on Meteor userId

--- imports/ui/containers/header/headerLeftSNP.js
+++ imports/ui/containers/header/headerLeftSNP.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import DeleteButton from '../../components/Buttons/DeleteButton'
-import muiThemeable from 'material-ui/styles/muiThemeable';
+import React, { Component } from "react";
+import DeleteButton from "../../components/Buttons/DeleteButton";
+import muiThemeable from "material-ui/styles/muiThemeable";
 
-const HeaderLeftSNP = (props) => (
-  <form style={{ color: props.muiTheme.palette.accent2Color }} className="header-left-snp">
+const HeaderLeftSNP = props => (
+  <form
+    style={{ color: props.muiTheme.palette.accent2Color }}
+    className="header-left-snp"
+  >
     <div>
       <label>Week</label>
-      <select></select>
+      <select />
     </div>
     <div>
       <label>Topic</label>
-      <select></select>
+      <select />
     </div>
   </form>
 );
 
-<<<<<<< HEAD:imports/ui/containers/header/HeaderLeftSNP.js
 export default muiThemeable()(HeaderLeftSNP);
-=======
-export default HeaderLeftSNP;
->>>>>>> 49947ef76918a81ae73a70de0370cd0e19237d12:imports/ui/containers/header/headerLeftSNP.js
