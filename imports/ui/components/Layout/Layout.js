0ace2307b7407e19414bf10d33f2aa18a3021950
Camden_Shaw <camden.shaw@gmail.com>
2017-12-01 14:32:04 -0800

    changed url back to localhost

--- imports/ui/components/Layout/Layout.js
+++ imports/ui/components/Layout/Layout.js
@@ -1,32 +1,32 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import "./styles.css"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import './styles.css'
 
-import HeaderContainer from "../../containers/header/header"
+import HeaderContainer from "../../containers/header/header";
 import Filters from "../../containers/filters/"
 
-export const url = `https://note-dot-js.herokuapp.com`
-const loginUrl = `${url}/login`
-const noteUrl = `${url}/note`
+export const url = `http://localhost:3000`;
+const loginUrl = `${url}/login`;
+const noteUrl = `${url}/note`;
 
 const Layout = ({ children }) => {
-    thisUrl = window.location.href
-    return (
-        <div className="app-wrapper">
-            {thisUrl !== loginUrl &&
-                thisUrl !== noteUrl && <HeaderContainer thisUrl={thisUrl} />}
-            {children}
-            <div className="app-footer" />
-        </div>
-    )
-}
+  thisUrl = window.location.href
+  return (
+    <div className="app-wrapper">
+      { thisUrl !== loginUrl && thisUrl !== noteUrl && <HeaderContainer thisUrl={thisUrl} /> }
+      {children}
+      <div className="app-footer">
+      </div>  
+    </div>
+  );
+};
 
 Layout.defaultProps = {
     children: null
-}
+};
 
 Layout.propTypes = {
     children: PropTypes.node
-}
+};
 
 export default Layout
--- imports/ui/containers/notes/notesList.js
+++ imports/ui/containers/notes/notesList.js
@@ -47,7 +47,7 @@ class NotesListContainer extends Component {
       // Meteor.userId() ?
       <div className="cards-container">
         {/* <Note /> */}
-        {window.location.href === "https://note-dot-js.herokuapp.com/profile" ? (
+        {window.location.href === "http://localhost:3000/profile" ? (
           this.state.sortedNotes.map((note, index) => (
               <SingleNote filters={this.props.filters} className="card" key={index} item={note} />
           ))
