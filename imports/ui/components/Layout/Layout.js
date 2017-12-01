d211a062861d78d3868e3afe2747ffc37c20ce4c
Camden_Shaw <camden.shaw@gmail.com>
2017-12-01 14:22:53 -0800

    changed urls from localhost to heroku

--- imports/ui/components/Layout/Layout.js
+++ imports/ui/components/Layout/Layout.js
@@ -1,32 +1,32 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import './styles.css'
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import "./styles.css"
 
-import HeaderContainer from "../../containers/header/header";
+import HeaderContainer from "../../containers/header/header"
 import Filters from "../../containers/filters/"
-/profile
-export const url = `https://note-dot-js.herokuapp.com`;
-const loginUrl = `${url}/login`;
-const noteUrl = `${url}/note`;
+
+export const url = `https://note-dot-js.herokuapp.com`
+const loginUrl = `${url}/login`
+const noteUrl = `${url}/note`
 
 const Layout = ({ children }) => {
-  thisUrl = window.location.href
-  return (
-    <div className="app-wrapper">
-      { thisUrl !== loginUrl && thisUrl !== noteUrl && <HeaderContainer thisUrl={thisUrl} /> }
-      {children}
-      <div className="app-footer">
-      </div>  
-    </div>
-  );
-};
+    thisUrl = window.location.href
+    return (
+        <div className="app-wrapper">
+            {thisUrl !== loginUrl &&
+                thisUrl !== noteUrl && <HeaderContainer thisUrl={thisUrl} />}
+            {children}
+            <div className="app-footer" />
+        </div>
+    )
+}
 
 Layout.defaultProps = {
     children: null
-};
+}
 
 Layout.propTypes = {
     children: PropTypes.node
-};
+}
 
 export default Layout
